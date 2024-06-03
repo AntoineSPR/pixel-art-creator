50fed1d5abd1dff5b12623ffdd86eec7a9dcb7d1
AntoineSPR <antoine.simper@gmail.com>
2024-06-03 17:55:14 +0200

    Co-authored-by: Irène Bouslama <IreneB21@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,2 +1,48 @@
 const app = document.getElementById('app');
-const canvasSize = 11;
+
+
+const reset = document.createElement('reset');
+reset.innerHTML = '<input type=reset>';
+app.append(reset);
+
+const submit = document.getElementById('submit');
+
+submit.addEventListener('click', (e)=> {
+    e.preventDefault();
+
+    let canvasSize = document.getElementById('canvas-size').value;
+
+    const table = document.createElement('table');
+    app.append(table);
+
+    let column = '';
+
+    for (let i = 0; i < canvasSize; i++) {
+        column += '<td></td>';
+    }
+
+    let row = '<tr>' + column + '</tr>';
+
+    for (let j = 0; j < canvasSize; j++) {
+        table.innerHTML += row;
+    }
+
+    const cell = table.getElementsByTagName('td');
+
+    for (let i = 0; i < cell.length; i++) {
+        cell[i].addEventListener('click', () => {
+            const color = document.getElementById('color').value;
+            if (cell[i].style.backgroundColor !== '' && cell[i].style.backgroundColor !== 'initial') {
+                cell[i].style.backgroundColor = 'initial';
+            } else {
+                cell[i].style.backgroundColor = color;
+            }
+        })
+    }
+
+    reset.addEventListener('click', (e) => {
+        for (let i = 0; i < cell.length; i++) {
+            cell[i].style.backgroundColor = 'initial';
+        }
+    })
+})
